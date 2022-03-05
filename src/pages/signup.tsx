86d4d9b6903486044962e69d09884e55b631eb11
oimo23 <oimo2338@gmail.com>
2022-03-05 20:50:55 +0900

    materialUIのフォームを実験

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -2,24 +2,16 @@ import { Button } from '@mui/material'
 import TextField from '@mui/material/TextField'
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
+import React from 'react'
 
 import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
-  const router = useRouter()
-
-  const loginClicked = () => {
-    // ログインAPIを叩き、200が帰ってくればindexへ遷移する
-    router.push('/')
-  }
-
   return (
     <div className={styles.container}>
       <Head>
-        <title>ログイン</title>
-        <meta name="description" content="ログインページ。" />
+        <title>登録する</title>
+        <meta name="description" content="登録する" />
       </Head>
 
       <main className={styles.main}>
@@ -38,13 +30,9 @@ const Home: NextPage = () => {
         />
 
         <p>
-          <Button className="mt-4" variant="contained" onClick={loginClicked}>
-            ログイン
+          <Button className="mt-4" variant="contained">
+            登録
           </Button>
-        </p>
-
-        <p>
-          アカウントをお持ちでない方は<Link href="/signup">新規登録</Link>
         </p>
       </main>
     </div>
